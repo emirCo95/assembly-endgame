f66d51f37914130c7fbcd2f81f3631f77aa5f929
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-01-04 17:48:30 -0500

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 const Header = () => {
   return (
-    <div className="header-container">
+    <header className="header-container">
       <h1 className="header-title">Assembly: Endgame</h1>
       <p className="header-info">
         Guess the word in under 8 attempts to keep the programming world safe
         from Assembly!
       </p>
-    </div>
+    </header>
   );
 };
 
