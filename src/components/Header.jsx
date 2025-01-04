dcf42ca0e02014ca7a9408b7eda8f55e656aae7c
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-01-04 17:42:59 -0500

    create Header.jsx component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1 className="header-title">Assembly: Endgame</h1>
+      <p className="header-info">
+        Guess the word in under 8 attempts to keep the programming world safe
+        from Assembly!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
