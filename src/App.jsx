5d1c360c52eea576a1603ce9f7c5bc16c1ef2474
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-01-04 18:00:28 -0500

    create Status.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Status from './components/Status';
 
 const App = () => {
   return (
     <main>
       <Header />
+      <Status />
     </main>
   );
 };
---
+++ src/components/Status.jsx
@@ -0,0 +1,10 @@
+const Status = () => {
+  return (
+    <div className="status-container">
+      <h1>You Win!</h1>
+      <p>Well Done! 🥳</p>
+    </div>
+  );
+};
+
+export default Status;
