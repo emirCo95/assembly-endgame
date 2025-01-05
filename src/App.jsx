4e1c39847df1a647602b4a54ce07bba49feb2e63
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-01-05 11:21:01 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
 import Language from './components/Language';
 import Status from './components/Status';
@@ -5,6 +7,8 @@ import Status from './components/Status';
 import { languages } from './utils/languages';
 
 const App = () => {
+  const [currentWord, setCurrentWord] = useState('react');
+
   const languageElements = languages.map((language, idx) => {
     return (
       <Language
