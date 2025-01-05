85060ba4b954fc10449a4a77d510739c762efe64
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-01-05 11:59:39 -0500

    update Word.jsx

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -4,11 +4,11 @@ import Letter from './Letter';
 
 const Word = (props) => {
   return (
-    <div className="word-container">
+    <section className="word-container">
       {props.word.split('').map((letter, idx) => {
         return <Letter key={idx} char={letter} />;
       })}
-    </div>
+    </section>
   );
 };
 
