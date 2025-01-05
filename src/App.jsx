057d23a6a227040f76c5076b3b6872a06841e343
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-01-05 10:28:29 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import Header from './components/Header';
+import Language from './components/Language';
 import Status from './components/Status';
+
+import { languages } from './utils/languages';
+
+const languageElements = languages.map((language, idx) => {
+  <Language
+    key={idx}
+    name={language.name}
+    backgroundColor={language.backgroundColor}
+    color={language.color}
+  />;
+});
 
 const App = () => {
   return (
     <main>
       <Header />
       <Status />
+      {languageElements}
     </main>
   );
 };
